ebb4f57cc759aef1a9efca24a07869bab829b229
Kevin McCarthy <me@kevinmccarthy.org>
2013-11-12 17:36:25 -1000

    Update middleware.py

--- oauthadmin/middleware.py
+++ oauthadmin/middleware.py
@@ -1,16 +1,10 @@
-from django.contrib.auth.middleware import AuthenticationMiddleware
-from django.contrib.sessions.middleware import SessionMiddleware
 from django.conf import settings as dj_settings
 
-def _starts_with_list(str, list):
-    for item in list:
-        if str.startswith(item):
-            return True
-    return False
 
 class OauthAdminSessionMiddleware(object):
     def process_request(self,request):
-        SessionMiddleware().process_request(request)
+        if not hasattr(request,' session'):
+            raise Exception("Make sure you put the OuathAdminmiddleware *after* the django session middleware"
         if 'user' in request.session:
             request.user = request.session['user']
             request._cached_user = request.session['user']
