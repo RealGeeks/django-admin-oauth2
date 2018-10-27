f389a5ca586859685c63b7f01ba2eadb736942f7
Jorge Puente-Sarrín <puentesarrin@gmail.com>
2018-10-27 00:14:59 -0500

    Make cleanup on default settings.

--- oauthadmin/settings.py
+++ oauthadmin/settings.py
@@ -1,13 +1,17 @@
-from django.conf import settings as global_settings
+from django.conf import settings as django_settings
 
-defaults = {
-    # default values for all django-oauth2-admin settings
-    "GET_USER": 'oauthadmin.stubs.get_user',
-    "PING_INTERVAL": 300,
-    "OAUTHADMIN_DEFAULT_NEXT_URL": "/admin/",
+# Default values for all django-oauth2-admin settings
+DEFAULT_SETTINGS = {
+    'GET_USER': 'oauthadmin.stubs.get_user',
+    'PING_INTERVAL': 300,
+    'OAUTHADMIN_DEFAULT_NEXT_URL': '/admin/',
 }
+OAUTHADMIN_SETTINGS_PREFIX = 'OAUTHADMIN_'
 
-global_prefix = 'OAUTHADMIN_'
 
 def app_setting(name):
-    return getattr(global_settings, global_prefix+name, defaults.get(name))
+    return getattr(
+        django_settings,
+        '{}{}'.format(OAUTHADMIN_SETTINGS_PREFIX, name),
+        DEFAULT_SETTINGS.get(name)
+    )
