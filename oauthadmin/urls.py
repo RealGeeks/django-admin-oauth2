ec3e3b600c9086c68e97540ec7e18a0b32f7246b
Lars Levie <larslevie@gmail.com>
2023-11-29 16:47:34 -0600

    make Django 4 support backwards compatible

--- oauthadmin/urls.py
+++ oauthadmin/urls.py
@@ -1,9 +1,14 @@
-from django.urls import re_path
 import oauthadmin.views
 
+try:
+    from django.urls import re_path as url
+except ImportError:
+    from django.conf.urls import url
+
+
 urlpatterns = [
-    re_path(r'login/', oauthadmin.views.login),
-    re_path(r'callback/', oauthadmin.views.callback),
-    re_path(r'logout/', oauthadmin.views.logout),
-    re_path(r'logout_redirect/', oauthadmin.views.logout_redirect),
+    url(r"login/", oauthadmin.views.login),
+    url(r"callback/", oauthadmin.views.callback),
+    url(r"logout/", oauthadmin.views.logout),
+    url(r"logout_redirect/", oauthadmin.views.logout_redirect),
 ]
